3ef62712ef6ac12bd86b1bd65ef25298eb29475f
yizhishuai <liushuaidejia@163.com>
2019-09-03 22:13:29 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import fastClick from 'fastclick'
 import router from './router'
+import './assets/style/reset.css'
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
