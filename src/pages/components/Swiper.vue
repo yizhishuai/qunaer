8ef96449bd7c32b0e32d3e30a0a801b17eadba47
yizhishuai <liushuaidejia@163.com>
2019-09-12 20:44:16 +0800

    轮播图完成

---
+++ src/pages/components/Swiper.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="swiper">
+	<swiper :options="swiperOption">
+    <swiper-slide><img class="img-swiper" src="https://images.pexels.com/photos/2884462/pexels-photo-2884462.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" ></swiper-slide>
+    <swiper-slide><img class="img-swiper" src="https://images.pexels.com/photos/1438072/pexels-photo-1438072.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"></swiper-slide>
+    <swiper-slide><img class="img-swiper" src="https://images.pexels.com/photos/2884572/pexels-photo-2884572.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"></swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+    </div>
+</template>
+<script>
+export default{
+	name:'HomeSwiper',
+	data(){
+		return {
+			swiperOption:{
+		    pagination: '.swiper-pagination',
+            loop:true
+	      }
+	      	
+	   }
+	}
+}	
+</script>
+<style lang="stylus" scoped>
+#swiper >>> .swiper-pagination-bullet-active
+  background:#fff
+#swiper
+  overflow:hidden
+  width:100%
+  .img-swiper
+    width:100%
+    margin: -50px 0px -50px; 
+	 overflow:hidden
+</style>
